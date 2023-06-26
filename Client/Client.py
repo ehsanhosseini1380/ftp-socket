bf0ef6ca88b6e7da9112335c0b41217d4bac1ef6
pegahmn <pegahmotahari@gmail.com>
2023-06-26 16:11:24 +0330

    init

---
+++ Client/Client.py
@@ -0,0 +1,252 @@
+import socket
+import sys
+import os
+import struct
+
+TCP_IP = "127.0.0.1"  # Only a local server
+TCP_PORT = 1456  # Just a random choice
+BUFFER_SIZE = 1024  # Standard choice
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def conn():
+    # Connect to the server
+    print("Sending server request...")
+    try:
+        s.connect((TCP_IP, TCP_PORT))
+        print("Connection successful")
+    except Exception as e:
+        print("Connection unsuccessful. Make sure the server is online.")
+        print(f"Error: {str(e)}")
+
+def upld(file_name):
+    # Upload a file
+    print("\nUploading file: {}...".format(file_name))
+    try:
+        # Check if the file exists
+        if not os.path.isfile(file_name):
+            print("Couldn't open file. Make sure the file name was entered correctly.")
+            return
+        # Make upload request
+        s.sendall(b"UPLD")
+    except Exception as e:
+        print("Couldn't make server request. Make sure a connection has been established.")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Wait for server acknowledgement then send file details
+        # Wait for server ok
+        s.recv(BUFFER_SIZE)
+        # Send file name size and file name
+        file_name_size = struct.pack("h", len(file_name.encode()))
+        s.send(file_name_size)
+        s.send(file_name.encode())
+        # Wait for server ok then send file size
+        s.recv(BUFFER_SIZE)
+        file_size = os.path.getsize(file_name)
+        s.send(struct.pack("i", file_size))
+    except Exception as e:
+        print("Error sending file details")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Send the file in chunks defined by BUFFER_SIZE
+        # Doing it this way allows for unlimited potential file sizes to be sent
+        with open(file_name, "rb") as content:
+            print("\nSending...")
+            while True:
+                chunk = content.read(BUFFER_SIZE)
+                if not chunk:
+                    break
+                s.send(chunk)
+            # Get upload performance details
+            upload_time = struct.unpack("f", s.recv(4))[0]
+            upload_size = struct.unpack("i", s.recv(4))[0]
+            print("\nSent file: {}\nTime elapsed: {}s\nFile size: {}b".format(file_name, upload_time, upload_size))
+    except Exception as e:
+        print("Error sending file")
+        print(f"Error: {str(e)}")
+        return
+
+def list_files():
+    # List the files available on the file server
+    print("Requesting files...\n")
+    try:
+        # Send list request
+        s.sendall(b"LIST")
+    except Exception as e:
+        print("Couldn't make server request. Make sure a connection has been established.")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # First get the number of files in the directory
+        number_of_files = struct.unpack("i", s.recv(4))[0]
+        # Then enter into a loop to receive details of each, one by one
+        for _ in range(number_of_files):
+            # Get the file name size first to slightly lessen the amount transferred over the socket
+            file_name_size = struct.unpack("i", s.recv(4))[0]
+            file_name = s.recv(file_name_size).decode()
+            # Also get the file size for each item on the server
+            file_size = struct.unpack("i", s.recv(4))[0]
+            print("\t{} - {}b".format(file_name, file_size))
+            # Make sure that the client and server are synchronized
+            s.sendall(b"1")
+        # Get total size of the directory
+        total_directory_size = struct.unpack("i", s.recv(4))[0]
+        print("Total directory size: {}b".format(total_directory_size))
+    except Exception as e:
+        print("Couldn't retrieve listing")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Final check
+        s.sendall(b"1")
+        return
+    except Exception as e:
+        print("Couldn't get final server confirmation")
+        print(f"Error: {str(e)}")
+        return
+
+def dwld(file_name):
+    # Download given file
+    print("Downloading file: {}".format(file_name))
+    try:
+        # Send server request
+        s.sendall(b"DWLD")
+    except Exception as e:
+        print("Couldn't make server request. Make sure a connection has been established.")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Wait for server ok, then make sure file exists
+        s.recv(BUFFER_SIZE)
+        # Send file name length, then name
+        file_name_size = struct.pack("h", len(file_name.encode()))
+        s.send(file_name_size)
+        s.send(file_name.encode())
+        # Get file size (if exists)
+        file_size = struct.unpack("i", s.recv(4))[0]
+        if file_size == -1:
+            # If file size is -1, the file does not exist
+            print("File does not exist. Make sure the name was entered correctly.")
+            return
+    except Exception as e:
+        print("Error checking file")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Send ok to receive file content
+        s.sendall(b"1")
+        # Enter loop to receive file
+        with open(file_name, "wb") as output_file:
+            bytes_received = 0
+            print("\nDownloading...")
+            while bytes_received < file_size:
+                # Again, file broken into chunks defined by the BUFFER_SIZE variable
+                chunk = s.recv(BUFFER_SIZE)
+                output_file.write(chunk)
+                bytes_received += len(chunk)
+        print("Successfully downloaded {}".format(file_name))
+        # Tell the server that the client is ready to receive the download performance details
+        s.sendall(b"1")
+        # Get performance details
+        time_elapsed = struct.unpack("f", s.recv(4))[0]
+        print("Time elapsed: {}s\nFile size: {}b".format(time_elapsed, file_size))
+    except Exception as e:
+        print("Error downloading file")
+        print(f"Error: {str(e)}")
+        return
+
+def delf(file_name):
+    # Delete specified file from the file server
+    print("Deleting file: {}...".format(file_name))
+    try:
+        # Send request, then wait for go-ahead
+        s.sendall(b"DELF")
+        s.recv(BUFFER_SIZE)
+    except Exception as e:
+        print("Couldn't connect to server. Make sure a connection has been established.")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Send file name length, then file name
+        file_name_size = struct.pack("h", len(file_name.encode()))
+        s.send(file_name_size)
+        s.send(file_name.encode())
+    except Exception as e:
+        print("Couldn't send file details")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Get confirmation that file does/doesn't exist
+        file_exists = struct.unpack("i", s.recv(4))[0]
+        if file_exists == -1:
+            print("The file does not exist on the server")
+            return
+    except Exception as e:
+        print("Couldn't determine file existence")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Confirm user wants to delete the file
+        confirm_delete = input("Are you sure you want to delete {}? (Y/N)\n".format(file_name)).upper()
+        # Make sure input is valid
+        # Unfortunately, Python doesn't have a do-while style loop, as that would have been better here
+        while confirm_delete != "Y" and confirm_delete != "N" and confirm_delete != "YES" and confirm_delete != "NO":
+            # If user input is invalid
+            print("Command not recognized, try again")
+            confirm_delete = input("Are you sure you want to delete {}? (Y/N)\n".format(file_name)).upper()
+    except Exception as e:
+        print("Couldn't confirm deletion status")
+        print(f"Error: {str(e)}")
+        return
+    try:
+        # Send confirmation
+        if confirm_delete == "Y" or confirm_delete == "YES":
+            # User wants to delete the file
+            s.sendall(b"Y")
+            # Wait for confirmation the file has been deleted
+            delete_status = struct.unpack("i", s.recv(4))[0]
+            if delete_status == 1:
+                print("File successfully deleted")
+                return
+            else:
+                # Client will probably send -1 to get here, but an else is used as more of a catch-all
+                print("File failed to delete")
+                return
+        else:
+            s.sendall(b"N")
+            print("Delete abandoned by user!")
+            return
+    except Exception as e:
+        print("Couldn't delete file")
+        print(f"Error: {str(e)}")
+        return
+
+def quit_ftp():
+    s.sendall(b"QUIT")
+    # Wait for server go-ahead
+    s.recv(BUFFER_SIZE)
+    s.close()
+    print("Server connection ended")
+    return
+
+print("\n\nWelcome to the FTP client.\n\nCall one of the following functions:\nCONN           : Connect to the server\nUPLD file_path : Upload a file\nLIST           : List files\nDWLD file_path : Download a file\nDELF file_path : Delete a file\nQUIT           : Exit")
+
+while True:
+    # Listen for a command
+    prompt = input("\nEnter a command: ")
+    if prompt[:4].upper() == "CONN":
+        conn()
+    elif prompt[:4].upper() == "UPLD":
+        upld(prompt[5:])
+    elif prompt[:4].upper() == "LIST":
+        list_files()
+    elif prompt[:4].upper() == "DWLD":
+        dwld(prompt[5:])
+    elif prompt[:4].upper() == "DELF":
+        delf(prompt[5:])
+    elif prompt[:4].upper() == "QUIT":
+        quit_ftp()
+        break
+    else:
+        print("Command not recognized; please try again")
---
+++ Server/Server.py
@@ -0,0 +1,193 @@
+import socket
+import sys
+import time
+import os
+import struct
+
+print("\nWelcome to the FTP server.\n\nTo get started, connect a client.")
+
+# Initialize socket stuff
+TCP_IP = "127.0.0.1"  # Only a local server
+TCP_PORT = 1456  # Just a random choice
+BUFFER_SIZE = 1024  # Standard size
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+conn, addr = s.accept()
+
+print("\nConnected to by address: {}".format(addr))
+
+
+def receive_data(size):
+    data = conn.recv(size)
+    if not data:
+        raise ConnectionError("Connection closed by client.")
+    return data
+
+
+def send_data(data):
+    conn.send(data)
+
+
+def upld():
+    # Send message once server is ready to receive file details
+    send_data(b"1")
+
+    # Receive file name length, then file name
+    file_name_size = struct.unpack("h", receive_data(2))[0]
+    file_name = receive_data(file_name_size)
+
+    # Send message to let client know server is ready for document content
+    send_data(b"1")
+
+    # Receive file size
+    file_size = struct.unpack("i", receive_data(4))[0]
+
+    # Initialize and enter loop to receive file content
+    start_time = time.time()
+    with open(file_name, "wb") as output_file:
+        print("\nReceiving...")
+        bytes_received = 0
+        while bytes_received < file_size:
+            data = receive_data(min(BUFFER_SIZE, file_size - bytes_received))
+            output_file.write(data)
+            bytes_received += len(data)
+    print("\nReceived file: {}".format(file_name))
+
+    # Send upload performance details
+    send_data(struct.pack("f", time.time() - start_time))
+    send_data(struct.pack("i", file_size))
+
+
+def list_files():
+    print("Listing files...")
+    # Get list of files in directory
+    file_list = os.listdir(os.getcwd())
+
+    # Send over the number of files, so the client knows what to expect (and avoid some errors)
+    send_data(struct.pack("i", len(file_list)))
+
+    total_directory_size = 0
+    for file_name in file_list:
+        file_name_bytes = file_name.encode()
+        file_size = os.path.getsize(file_name)
+        total_directory_size += file_size
+
+        # File name size
+        send_data(struct.pack("i", len(file_name_bytes)))
+
+        # File name
+        send_data(file_name_bytes)
+
+        # File content size
+        send_data(struct.pack("i", file_size))
+
+        # Make sure that the client and server are synchronized
+        receive_data(BUFFER_SIZE)
+
+    # Sum of file sizes in directory
+    send_data(struct.pack("i", total_directory_size))
+
+    # Final check
+    receive_data(BUFFER_SIZE)
+    print("Successfully sent file listing")
+
+
+def dwld():
+    send_data(b"1")
+
+    # Receive file name length and file name
+    file_name_length = struct.unpack("h", receive_data(2))[0]
+    file_name = receive_data(file_name_length)
+
+    if os.path.isfile(file_name):
+        # File exists, send file size
+        send_data(struct.pack("i", os.path.getsize(file_name)))
+    else:
+        # File doesn't exist, send error code
+        print("File name not valid")
+        send_data(struct.pack("i", -1))
+        return
+
+    # Wait for confirmation to send file
+    receive_data(BUFFER_SIZE)
+
+    # Enter loop to send file
+    start_time = time.time()
+    print("Sending file...")
+    with open(file_name, "rb") as content:
+        while True:
+            data = content.read(BUFFER_SIZE)
+            if not data:
+                break
+            send_data(data)
+
+    # Get client's go-ahead, then send download details
+    receive_data(BUFFER_SIZE)
+    send_data(struct.pack("f", time.time() - start_time))
+
+
+def delf():
+    # Send go-ahead
+    send_data(b"1")
+
+    # Get file details
+    file_name_length = struct.unpack("h", receive_data(2))[0]
+    file_name = receive_data(file_name_length)
+
+    # Check if file exists
+    if os.path.isfile(file_name):
+        send_data(struct.pack("i", 1))
+    else:
+        # File doesn't exist
+        send_data(struct.pack("i", -1))
+
+    # Wait for deletion confirmation
+    confirm_delete = receive_data(BUFFER_SIZE)
+    if confirm_delete == b"Y":
+        try:
+            # Delete file
+            os.remove(file_name)
+            send_data(struct.pack("i", 1))
+        except:
+            # Unable to delete file
+            print("Failed to delete {}".format(file_name))
+            send_data(struct.pack("i", -1))
+    else:
+        # User abandoned deletion
+        # The server probably received "N", but else used as a safety catch-all
+        print("Delete abandoned by client!")
+
+
+def quit_server():
+    # Send quit confirmation
+    send_data(b"1")
+
+    # Close the connection and the server
+    conn.close()
+    s.close()
+
+    # Restart the server
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+while True:
+    # Enter into a while loop to receive commands from the client
+    print("\n\nWaiting for instruction")
+    data = receive_data(BUFFER_SIZE)
+    print("\nReceived instruction: {}".format(data))
+
+    # Check the command and respond correctly
+    if data == b"UPLD":
+        upld()
+    elif data == b"LIST":
+        list_files()
+    elif data == b"DWLD":
+        dwld()
+    elif data == b"DELF":
+        delf()
+    elif data == b"QUIT":
+        quit_server()
+
+    # Reset the data to loop
+    data = None
